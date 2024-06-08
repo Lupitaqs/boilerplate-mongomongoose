ff99f5903653fc405292222eb003e4956e64d3cc
Lupitaqs <lupitaqs@outlook.com>
2024-06-07 19:24:15 -0600

    Install and set up mongoose

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
 
+const dotenv = require('dotenv');
+dotenv.config({path: 'sample.env'});
 
+const mongoose = require('mongoose');
 let Person;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.log('Error connecting to the database');
+  })
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
